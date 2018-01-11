53216d5d16ee844af1f3732a85c8ef1c2139f141
AzunaiML <gthtcels@mail.ru>
2018-01-11 11:00:24 +0300

    added check status function

--- autoupdater/server/status_server.py
+++ autoupdater/server/status_server.py
@@ -1,18 +1,32 @@
 import time
 import os
+import json
+from datetime import datetime
 
 from autoupdater.server import abstract_server
 
 
 class StatusServer(abstract_server.AbstractServer):
+    @staticmethod
+    def get_upd_date_file(path):
+        return datetime.strftime(datetime.fromtimestamp(os.path.getmtime(path)), '%d.%m.%Y %H:%M:%S')
 
     @staticmethod
     def check_status(path):
-        return path
+        msg = ''
+        for f in path:
+            if os.path.exists(f):
+                msg += "Файл %s: обновлен %s\n" % (os.path.split(f)[-1], str(StatusServer.get_upd_date_file(f)))
+            else:
+                msg += "Файл %s: не обновлен\n" % (os.path.split(f)[-1])
+        return msg
 
     def on_request(self, ch, method, props, body):
-        status = self.check_status(body)
+        body = body.decode("utf-8")
+        body = json.loads(body)
+        status = self.check_status(json.loads(body["path"]))
+        response = {"status": status, "chat_id": body["chat_id"]}
         self.channel.basic_publish(exchange='',
                                    routing_key=self.write_queue,
-                                   body=status)
+                                   body=json.dumps(response))
         time.sleep(1)
